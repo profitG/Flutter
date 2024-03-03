436a1b9234267973346064a260b510b53b91bfb4
profitG <dldbsry55@gmail.com>
2024-03-03 22:57:51 +0900

    erase

--- start_flutter/flutter_course_1/lib/main.dart
+++ start_flutter/flutter_course_1/lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_course_1/widgets/currencyCard.dart';
 import 'package:flutter_course_1/widgets/myButton.dart';
 
